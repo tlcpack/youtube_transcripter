7c86465d7d58a17340bdb243145d073d5c7794e8
Taylor Cooke <taylorcooke@gmail.com>
2021-03-05 13:09:43 -0500

    creates wordcloud

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 final = []
 finalstr = ''
@@ -13,4 +15,11 @@ finalstr = ' '.join(final)
 with open('text2.txt', mode='w') as file:
     file.write(finalstr)
     print("ready!")
-print(finalstr)
+
+# Create and generate a word cloud image:
+wordcloud = WordCloud().generate(finalstr)
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
